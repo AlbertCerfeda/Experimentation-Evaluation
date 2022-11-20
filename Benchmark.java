362f1af7094d58e048a317a6f84b9c8752979f4c
Alan Copa <copaa@usi.ch>
2022-11-20 18:31:07 +0100

    "possible change of notation"

--- Benchmark.java
+++ Benchmark.java
@@ -11,23 +11,25 @@ public class Benchmark {
 	private int actualRuns;
 	
 	
-	public Benchmark(Sorter algo, Integer[] input, int warmupRuns, int actualRuns){
+	public Benchmark(Sorter algo, Integer[] input, int warmupRuns, int actualRuns){ // int iterations
 		this.algo =algo;
 		this.input=input;
-		this.warmupRuns = warmupRuns;
-		this.actualRuns = actualRuns;
+		this.warmupRuns = warmupRuns; 
+		this.actualRuns = actualRuns; // remove
+		// this.iterations = iterations;
+		// just to follow the notation defined in the slides
 	}
 	
 	public BenchmarkResults runBenchmark() {
 		BenchmarkResults results = new BenchmarkResults(algo.getClass());
 		Integer[] newinput;
-		for (int i = 0; i < warmupRuns+actualRuns; i++) {
+		for (int i = 0; i < warmupRuns+actualRuns; i++) { // i < iterations; // since the global number iterations includes the warmupRuns (iterations = warmupRuns + actualRuns)
 			newinput = Arrays.copyOf(input, input.length);
 			
 			long startTime = System.nanoTime();
 			algo.sort(newinput);
 			long endTime = System.nanoTime();
-			
+
 			if(i<warmupRuns)
 				results.addWarmupResult(endTime-startTime);
 			else
